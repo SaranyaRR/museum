124e7dfc5bdaaff1b16113132843fc87ba522829
SaranyaRR <saranyar.inbox@gmail.com>
2016-08-29 12:16:06 +0530

    Change listing page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,20 +4,22 @@ class TicketsController < ApplicationController
   end
 
   def search
-  	@count = 0
+  	@tickets = nil
   	if params["search_date_time"]
-  		@count = Ticket.select{|ticket| params["search_date_time"].to_datetime.change(:offset =>Time.now.strftime("%:z")).between?(ticket.in_time, ticket.out_time || Time.now)}.count
+  		@tickets = Ticket.select{|ticket| params["search_date_time"].to_datetime.change(:offset =>Time.now.strftime("%:z")).between?(ticket.in_time, ticket.out_time || Time.now)}
   	end
-  	redirect_to root_url, notice: "#{@count} people were present in the museum in the given time."
   end
 
   def import
   	begin
+      @message = nil
   	  tickets_count, errors = Ticket.import(params[:file])
   	  if errors.empty?
-	      redirect_to root_url, notice: "#{tickets_count} tickets were successfully imported."
+        @tickets = Ticket.all
+	      @message = "#{tickets_count} tickets were successfully imported."
   		else
-			  redirect_to root_url, notice: "#{tickets_count} tickets were imported. One or more tickets were not imported. Error message : " + "#{errors}"
+        @tickets = Ticket.all
+			  @message = "#{tickets_count} tickets were imported. One or more tickets were not imported. Error message : " + "#{errors}"
   		end
     rescue
       redirect_to root_url, notice: "Import failed. Invalid file or the contents could not be parsed. Please verify and reupload again."
